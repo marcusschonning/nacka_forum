0637bfe17f5a96f2d57635628cd5daf9ebd250a8
Joakim Forslund <JoakimForslund@Joakims-MacBook-Pro.local>
2015-06-08 11:40:44 +0200

    Hejhej

--- insta.php
+++ insta.php
@@ -5,7 +5,7 @@ include 'connect.php';
 
 $limit = 50;
 $cid = '548b3af99cbc4d73b19781dcdcdb8d0c';
-$hash = 'nackaforummatochvin';
+$hash = 'mackan';
 $url = 'https://api.instagram.com/v1/tags/'.$hash.'/media/recent?client_id='.$cid.'&count='.$limit;
 $ret=json_decode(file_get_contents($url));
 $user_id=$ret->data[0]->id;
@@ -15,7 +15,10 @@ echo $ret->pagination->next_url;
 
 echo '<pre>', print_r($ret->pagination->next_url), '</pre>';
 
-
+while ($ret->pagination->next_url) {
+	$ret = json_decode(file_get_contents($ret->pagination->next_url));
+	echo "Hejsan";
+}
 
 $photos=@json_decode(file_get_contents($url));
 echo "Found: ".count($photos->data)."photos.<hr>";
