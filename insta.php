c4ba29d67f54d04175ee522e16f5e303b2bb9baa
Joakim Forslund <JoakimForslund@Joakims-MacBook-Pro.local>
2015-06-08 11:29:43 +0200

    Hejsan snoppar

---
+++ insta.php
@@ -0,0 +1,23 @@
+<?php 
+
+include 'connect.php';
+
+
+$limit = 50;
+$cid = '548b3af99cbc4d73b19781dcdcdb8d0c';
+$hash = 'nackaforummatochvin';
+$url = 'https://api.instagram.com/v1/tags/'.$hash.'/media/recent?client_id='.$cid.'&count='.$limit;
+$ret=json_decode(file_get_contents($url));
+$user_id=$ret->data[0]->id;
+
+
+echo $ret->pagination->next_url;
+
+echo '<pre>', print_r($ret->pagination->next_url), '</pre>';
+
+
+
+$photos=@json_decode(file_get_contents($url));
+echo "Found: ".count($photos->data)."photos.<hr>";
+?>
+
