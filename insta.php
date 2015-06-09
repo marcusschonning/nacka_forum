1474c5e7010c6a6fe040e24a48c112622028a141
marcusschonning <marcus.schonning@gmail.com>
2015-06-09 12:00:39 +0200

    insta.php typ klart!

--- insta.php
+++ insta.php
@@ -16,10 +16,12 @@ echo $ret->pagination->next_url;
 echo '<pre>', print_r($ret->pagination->next_url), '</pre>';
 
 $imgs = array();
+$img_link = array();
 
 foreach ($ret->data as $photo) {
 
-	$imgs[] = array('link'=>$photo->images->low_resolution->url, 'username'=>$photo->user->username);	
+	$imgs[] = array('link'=>$photo->images->low_resolution->url, 'username'=>$photo->user->username);
+	$img_link[] = $photo->images->low_resolution->url;
 
 }
 
@@ -28,40 +30,45 @@ while ($ret->pagination->next_url) {
 	$ret = json_decode(file_get_contents($ret->pagination->next_url));
 	foreach ($ret->data as $photo) {
 
-		$imgs[] = array('link'=>$photo->images->low_resolution->url, 'username'=>$photo->user->username);	
+		$imgs[] = array('link'=>$photo->images->low_resolution->url, 'username'=>$photo->user->username);
+		$img_link[] = $photo->images->low_resolution->url;
 
 	}
 }
-$counter = 0;
-$query = 'SELECT link from imgs';
+$db_imgs = array();
+$query = 'SELECT link, username from imgs';
 $result = $db->query($query);
 if($result->num_rows > 0){
 	while($row = $result->fetch_assoc()){
 		
-		//$row['link'];
-		for ($i=0; $i < count($imgs); $i++) { 
-			if($row['link'] === $imgs[$i]['link']){
-
-				//echo '<pre>', print_r($db_imgs), '</pre>';
-				//echo '<pre>', print_r($imgs), '</pre>';
-				$counter++;
-			}
-		}
-		
+		$db_imgs[] = $row['link'];
 	}
-	if($counter !== count($imgs)){
-			//töm databasen och släng in allt igen...
-			echo 'Töm. ';
-		}
 
-}else{
-	foreach ($imgs as $key => $value) {
-		$value['username'];
-		$stmt = $db->prepare('INSERT INTO imgs (link, username) VALUES (?, ?)');
-		$stmt->bind_param('ss', $value['link'], $value['username']);
-		$stmt->execute();
-		$stmt->close();
+}
+
+$img_diff = array_diff($img_link, $db_imgs);//ska in i databasen!
+$db_diff = array_diff($db_imgs, $img_link);//ska ut ur databasen!
+
+//Ta bort från databasen
+for ($i=0; $i < count($db_diff); $i++) { 
+	$stmt2 = $db->prepare("DELETE FROM imgs WHERE link = ?");
+	$stmt2->bind_param('s', $db_diff[$i]);
+	$stmt2->execute(); 
+	$stmt2->close();
+}
+	
+//Lägger till det som inte finns i databasen
+for($i = 0; $i < count($imgs); $i++){
+	for ($j=0; $j < count($img_diff); $j++) { 
+		if($imgs[$i]['link'] === $img_diff[$j]){
+			$stmt = $db->prepare('INSERT INTO imgs (link, username) VALUES (?, ?)');
+			$stmt->bind_param('ss', $imgs[$i]['link'], $imgs[$i]['username']);
+			$stmt->execute();
+			$stmt->close();
+		}
 	}
 }
+
+
 
 
