f93301ce573e7aeba91c003b01fb707b9b68bef4
Joakim Forslund <JoakimForslund@Joakims-MacBook-Pro.local>
2015-08-31 14:41:56 +0200

    Style on startpage

---
+++ header.php
@@ -0,0 +1,9 @@
+<nav class="main-navigation">
+	<ul>
+		<li><a href="index.php" class="active">TÄVLA</a></li>
+		<li><a href="all.php">ALLA BIDRAG</a></li>
+		<li><a href="toplist.php">TOPPLISTA</a></li>
+		<li><a href="winners.php">VINNARE</a></li>
+		<li><a href="rules.php">REGLER &#38; PRISER</a></li>
+	</ul>
+</nav>
--- index.php
+++ index.php
@@ -3,20 +3,48 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Nacka Forum</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-<h1>Pung</h1>
+	<div id="wrapper">
+	<?php include "header.php"; ?>
+		<div class="intro">
+			<h2>Ladda upp din bästa matbild</h2>
+			<h3>Vinn en matkasse med varor utvalda av kocken Tommy Myllymäki</h3>
+			<p>Följ instruktionerna nedan för att delta i tävlingen. 
+			Här i appen presenteras alla tävlingsbidrag som laddats upp via Instagram, 
+			och här kan du också rösta på dina favoritbilder. Varje vecka utser en jury, 
+			som består av bland annat Tommy Myllymäki, 4 vinnare. 
+			Dessa vinner en varsin lyxig matkasse med produkter utvalda av koken Tommy 
+			Myllemäki till ett värde av 1200 kr. Du kan delta med bild och rösta fram tom den 3 november 2013.
+			Läs mer om regler och priserna här. Lycka till!</p>
 
-<?php 
-session_start();
+			<ul class="steps clearfix">
+				<li><span class="number">1</span><span class="description"><h6>Fota din maträtt</h6><p>Ta ett foto med din mobil på en maträtt du köpt i någon av Nacka Forums caféer eller restauranger eller som du tillagat hemma tex. till middag</p></span></li>
+				<li><span class="number">2</span><span class="description"><h6>Ladda upp fotot på instagram</h6><p>Följ @nackaforum på Instagram och se till att ditt konto är publikt</p></span></li>
+				<li><span class="number">3</span><span class="description"><h6>Tagga</h6><p>Tagga bilden med: #nackaforummatochvin</p></span></li>
+				<li><span class="number">4</span><span class="description"><h6>Rösta</h6><p>På ditt eller andras bidrag via Nacka Forums Facebooksida</p></span></li>
+			</ul>
 
-if (isset($_SESSION['graphobject'])) {
-	echo "Inloggad! :D";
+		</div>
 
-	echo "<pre>" , print_r($_SESSION['graphobject']) , "</pre>";
-}else{
-	echo "Inte inloggad";
-}
-?>
+
+
+
+
+
+		<?php 
+		session_start();
+
+		if (isset($_SESSION['graphobject'])) {
+			echo "Inloggad! :D";
+
+			echo "<pre>" , print_r($_SESSION['graphobject']) , "</pre>";
+		}else{
+			echo "Inte inloggad";
+		}
+		?>
+		<?php include "footer.php"; ?>
+	</div>
 </body>
 </html>
